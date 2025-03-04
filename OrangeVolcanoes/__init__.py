2f42589b501f10dad49a73466b056b6672932009
Marko Toplak <marko.toplak@gmail.com>
2025-03-04 16:22:41 +0100

    Set proper documentation entry-points

--- OrangeVolcanoes/__init__.py
+++ OrangeVolcanoes/__init__.py
@@ -1,3 +1,5 @@
+import sysconfig
+
 import Orange.data
 import os.path
 import os
@@ -12,4 +14,21 @@ ICON = 'icons/Volcano.png'
 
 Orange.data.table.dataset_dirs.append(get_sample_datasets_dir())
 
-WIDGET_HELP_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__),'..','docs','build','html'))
+WIDGET_HELP_PATH = (
+    # Development documentation (in editable mode)
+    # You need to build help pages manually using
+    # make html
+    # inside docs/ folder
+    ("{DEVELOP_ROOT}/docs/build/html/index.html", None),
+
+    # Documentation included in wheel
+    # Correct DATA_FILES entry is needed in setup.py and documentation has to be built
+    # before the wheel is created.
+    ("{}/help/orange3-volcanoes/index.html".format(sysconfig.get_path("data")), None),
+
+    # Online documentation url, used when the local documentation is available.
+    # Url should point to a page with a section Widgets. This section should
+    # include links to documentation pages of each widget. Matching is
+    # performed by comparing link caption to widget name.
+    ("https://orange3-volcanoes.readthedocs.io/en/latest/", ""),
+)
--- setup.py
+++ setup.py
@@ -4,5 +4,4 @@ setup(name="Volcanoes",
       packages=["OrangeVolcanoes"],
       package_data={"OrangeVolcanoes": ["icons/*", "doc/*, datasets/*"]},
       classifiers=["Example :: Invalid"],
-      entry_points={"orange.widgets": "Volcanoes = OrangeVolcanoes"},
       )
